74516695ba8abc06e7feaf6c617ce7ce014ee7bf
Usuario <Usuario@CPD02-M03>
2024-05-28 11:53:54 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppX/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppX
+{
+    internal class Program
+    {
+        static void calculo(double x, double y, string calc)
+        {
+            double z = 0;
+            switch (calc)
+            {
+                case "+":
+                    z = x + y;
+                    break;
+
+                case "-":
+                    z = x - y;
+                    break;
+
+                case "*":
+                    z = x * y;
+                    break;
+
+                case "/":
+                    z = x / y;
+                    break;
+            }
+
+            Console.Write("O resultado é: " + z);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o primeiro número: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira o segundo número: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a operação\n+ -> Soma\n- -> Subtração\n* -> Multiplicação\n/ -> Divisão");
+            string calc = Console.ReadLine().Substring(0, 1);
+
+            calculo(x, y, calc);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FuncaoParametrosII/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoParametrosII
+{
+    internal class Program
+    {
+        static double calculo(double x, double y, string calc)
+        {
+            switch (calc)
+            {
+                case "+":
+                    return x + y;
+
+                case "-":
+                    return x - y;
+
+                case "*":
+                    return x * y;
+
+                case "/":
+                    return x / y;
+
+                default:
+                    return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o primeiro número: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira o segundo número: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a operação\n+ -> Soma\n- -> Subtração\n* -> Multiplicação\n/ -> Divisão");
+            string calc = Console.ReadLine().Substring(0, 1);
+
+            Console.WriteLine("O resultado é: " + calculo(x, y, calc));
+
+            Console.ReadKey();
+        }
+    }
+}
